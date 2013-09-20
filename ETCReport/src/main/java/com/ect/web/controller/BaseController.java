4ee537b14a38cad1ecfff1465ee35d8a65d90064
totoland29 <totoland29@gmail.com>
2013-09-20 20:48:22 +0000

    Create new project

---
+++ ETCReport/src/main/java/com/ect/web/controller/BaseController.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ect.web.controller;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Totoland
+ */
+public abstract class BaseController implements Serializable{
+    private static final long serialVersionUID = -3424239608725799082L;
+
+}
