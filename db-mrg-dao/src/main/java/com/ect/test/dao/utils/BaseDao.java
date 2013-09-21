b4813b4cad94d3c4b27e4de3fd020d8ab3c770a9
totoland29 <totoland29@gmail.com>
2013-09-21 22:32:48 +0000

    create base DAO

--- db-mrg-dao/src/main/java/com/ect/test/dao/utils/BaseDao.java
+++ db-mrg-dao/src/main/java/com/ect/test/dao/utils/BaseDao.java
@@ -4,7 +4,7 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
-public abstract class CustomHibernateDaoSupport extends HibernateDaoSupport
+public abstract class BaseDao extends HibernateDaoSupport
 {    
 	@Autowired
     public void anyMethodName(SessionFactory sessionFactory)
