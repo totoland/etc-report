bb6104b5d53844057f2c8739940edd335fd5ad04
totoland29@gmail.com <totoland29@gmail.com@cf129479-eaf2-b3b5-df12-51732378df0f>
2014-02-13 15:32:07 +0000

    add report013

--- trunk/ETCReport/src/main/webapp/resources/js/script.js
+++ trunk/ETCReport/src/main/webapp/resources/js/script.js
@@ -1341,15 +1341,3 @@ PrimeFaces.locales['th'] = {
 
 };
 
-function inputNumber(event) {
-    // Allow only backspace and delete
-    if (event.keyCode == 46 || event.keyCode == 8) {
-        // let it happen, don't do anything
-    }
-    else {
-        // Ensure that it is a number and stop the keypress
-        if (event.keyCode < 48 || event.keyCode > 57) {
-            event.preventDefault();
-        }
-    }
-}
