16c7666a0b4a9b8d4afd38fff55177ef079749d1
totoland29 <totoland29@gmail.com>
2014-02-18 17:26:16 +0000

    add report017

---
+++ db-mrg-dao/src/main/java/com/ect/db/report/dao/Report017Dao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ect.db.report.dao;
+
+import com.ect.db.report.entity.Report017;
+
+/**
+ *
+ * @author totoland
+ */
+public interface Report017Dao {
+    
+    Report017 findByReportId(Integer paramReportId);
+    
+}
