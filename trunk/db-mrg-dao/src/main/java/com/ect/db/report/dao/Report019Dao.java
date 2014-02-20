f7edd3e0ab322eb6f9d13ab9d10d799d330fc291
totoland29@gmail.com <totoland29@gmail.com@cf129479-eaf2-b3b5-df12-51732378df0f>
2014-02-20 04:59:31 +0000

    add report019

---
+++ trunk/db-mrg-dao/src/main/java/com/ect/db/report/dao/Report019Dao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ect.db.report.dao;
+
+import com.ect.db.report.entity.Report019;
+
+/**
+ *
+ * @author totoland
+ */
+public interface Report019Dao {
+    
+    Report019 findByReportId(Integer paramReportId);
+    
+}
---
+++ trunk/db-mrg-dao/src/main/java/com/ect/db/report/entity/Report019.java
@@ -0,0 +1,247 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ect.db.report.entity;
+
+import com.ect.db.domain.entity.DomainEntity;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import org.hibernate.annotations.Cascade;
+
+/**
+ *
+ * @author totoland
+ */
+@Entity
+@Table(name = "REPORT_019")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Report019.findAll", query = "SELECT r FROM Report019 r")})
+public class Report019 extends DomainEntity implements Serializable {
+    private static final long serialVersionUID = -2473337884920287862L;
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "REPORT_ID")
+    private Integer reportId;
+    @Column(name = "REPORT_CODE")
+    private String reportCode;
+    @Column(name = "REPORT_DESC")
+    private String reportDesc;
+    @Column(name = "CREATED_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    @Column(name = "CREATED_USER")
+    private Integer createdUser;
+    @Column(name = "CREATED_USER_GROUP")
+    private Integer createdUserGroup;
+    @Column(name = "UPDATED_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedDate;
+    @Column(name = "UPDATED_USER")
+    private Integer updatedUser;
+    @Column(name = "FLOW_STATUS_ID")
+    private Integer flowStatusId;
+    @Column(name = "REPORT_STATUS")
+    private Integer reportStatus;
+    @Lob
+    @Column(name = "REMARK")
+    private String remark;
+    @Column(name = "REJECTED_USER")
+    private Integer rejectedUser;
+    @Column(name = "REJECTED_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date rejectedDate;
+    @Column(name = "APPROVED_USER")
+    private Integer approvedUser;
+    @Column(name = "APPROVED_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date approvedDate;
+    @OneToMany(mappedBy = "reportId",cascade = CascadeType.ALL,targetEntity = Report019Detail.class,fetch = FetchType.EAGER)
+    @Cascade(org.hibernate.annotations.CascadeType.DELETE_ORPHAN)
+    private List<Report019Detail> report019DetailList;
+
+    public Report019() {
+    }
+
+    public Report019(Integer reportId) {
+        this.reportId = reportId;
+    }
+
+    public Integer getReportId() {
+        return reportId;
+    }
+
+    public void setReportId(Integer reportId) {
+        this.reportId = reportId;
+    }
+
+    public String getReportCode() {
+        return reportCode;
+    }
+
+    public void setReportCode(String reportCode) {
+        this.reportCode = reportCode;
+    }
+
+    public String getReportDesc() {
+        return reportDesc;
+    }
+
+    public void setReportDesc(String reportDesc) {
+        this.reportDesc = reportDesc;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Integer getCreatedUser() {
+        return createdUser;
+    }
+
+    public void setCreatedUser(Integer createdUser) {
+        this.createdUser = createdUser;
+    }
+
+    public Integer getCreatedUserGroup() {
+        return createdUserGroup;
+    }
+
+    public void setCreatedUserGroup(Integer createdUserGroup) {
+        this.createdUserGroup = createdUserGroup;
+    }
+
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public Integer getUpdatedUser() {
+        return updatedUser;
+    }
+
+    public void setUpdatedUser(Integer updatedUser) {
+        this.updatedUser = updatedUser;
+    }
+
+    public Integer getFlowStatusId() {
+        return flowStatusId;
+    }
+
+    public void setFlowStatusId(Integer flowStatusId) {
+        this.flowStatusId = flowStatusId;
+    }
+
+    public Integer getReportStatus() {
+        return reportStatus;
+    }
+
+    public void setReportStatus(Integer reportStatus) {
+        this.reportStatus = reportStatus;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public Integer getRejectedUser() {
+        return rejectedUser;
+    }
+
+    public void setRejectedUser(Integer rejectedUser) {
+        this.rejectedUser = rejectedUser;
+    }
+
+    public Date getRejectedDate() {
+        return rejectedDate;
+    }
+
+    public void setRejectedDate(Date rejectedDate) {
+        this.rejectedDate = rejectedDate;
+    }
+
+    public Integer getApprovedUser() {
+        return approvedUser;
+    }
+
+    public void setApprovedUser(Integer approvedUser) {
+        this.approvedUser = approvedUser;
+    }
+
+    public Date getApprovedDate() {
+        return approvedDate;
+    }
+
+    public void setApprovedDate(Date approvedDate) {
+        this.approvedDate = approvedDate;
+    }
+
+    @XmlTransient
+    public List<Report019Detail> getReport019DetailList() {
+        return report019DetailList;
+    }
+
+    public void setReport019DetailList(List<Report019Detail> report019DetailList) {
+        this.report019DetailList = report019DetailList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (reportId != null ? reportId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Report019)) {
+            return false;
+        }
+        Report019 other = (Report019) object;
+        if ((this.reportId == null && other.reportId != null) || (this.reportId != null && !this.reportId.equals(other.reportId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Report019{" + "reportId=" + reportId + ", reportCode=" + reportCode + ", reportDesc=" + reportDesc + ", createdDate=" + createdDate + ", createdUser=" + createdUser + ", createdUserGroup=" + createdUserGroup + ", updatedDate=" + updatedDate + ", updatedUser=" + updatedUser + ", flowStatusId=" + flowStatusId + ", reportStatus=" + reportStatus + ", remark=" + remark + ", rejectedUser=" + rejectedUser + ", rejectedDate=" + rejectedDate + ", approvedUser=" + approvedUser + ", approvedDate=" + approvedDate + ", report019DetailList=" + report019DetailList + '}';
+    }
+    
+}
